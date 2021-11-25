2f73f554f546c415ed146a069bba0dc3b9f59f37
DuCheAnh <ducheanh1308@gmail.com>
2021-11-25 16:16:23 +0700

    fixes: movements

--- MobileCoop/Scripts/Player.gd
+++ MobileCoop/Scripts/Player.gd
@@ -9,6 +9,15 @@ onready var sprite=$AnimatedSprite
 var velocity = Vector2.ZERO
 var was_on_floor=false
 
+func _physics_process(delta):
+	_get_input()
+	_apply_animation()
+	_normalize_animation_scale(delta)
+	was_on_floor=is_on_floor()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+
 func _get_input():
 	velocity.x = 0
 	if Input.is_action_pressed("ui_right"):
@@ -38,14 +47,6 @@ func _apply_animation():
 		sprite.scale = Vector2(1.1, 0.9)
 func _normalize_animation_scale(delta):
 	sprite.scale = sprite.scale.linear_interpolate(Vector2(1, 1), delta * 12)
-func _physics_process(delta):
-	_get_input()
-	_apply_animation()
-	_normalize_animation_scale(delta)
-	was_on_floor=is_on_floor()
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-
 
 func jump(multiplier):
 	sprite.scale = Vector2(0.75, 1.25)
